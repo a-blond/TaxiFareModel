35f12c5d98ce95e41e94e8b9a062e7f042e21bfa
Aurelien Blond <aurelien.blond@gmail.com>
2021-06-22 16:34:10 +0200

    Connected to ML Flow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -9,8 +9,14 @@ from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
 import pandas as pd
 
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
 
-class Trainer():
+class Trainer():   
+    MLFLOW_URI = "https://mlflow.lewagon.co/"   
+    EXPERIMENT_NAME = "[FR] [Bordeaux] [a-blond] TaxiFareModel + v1" 
+    
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -19,6 +25,29 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+    
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -54,6 +83,11 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         print(rmse)
+        
+        #log into MLFlow
+        self.mlflow_log_param("model", "Linear_regression")
+        self.mlflow_log_metric("RMSE", rmse)
+
         return rmse
 
 
